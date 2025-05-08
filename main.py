4539703a0400adfbc60b568ba97d20ec85ec4794
OscarCoxon7771 <ramirezoscardavid.2005@gmail.com>
2025-05-07 20:07:25 -0500

    7 de mayo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 
 
@@ -26,16 +26,44 @@ def ruta_consulta():
     return f"Se está consultando el producto {producto} de talla {talla}"
 
 #Ruta para capturar datos por el cuerpo de solicitud para el body
-
+listado=[]
 @app.route("/registro", methods = ["GET"])
 def ruta_registro():
-    return render_template("formulario.html")
+   # listado = [{"Nombre":"Juanito", "Correo":"juan@gmail.com"}]
+    return render_template("formulario.html", listado=listado)
 
 @app.route("/registro", methods = ["POST"])
 def procesar_registro():
     nombre=request.form.get("Nombre")
     correo=request.form.get("Correo")
+    estudiantes={"Nombre":nombre, "Correo":correo}
+    listado.append(estudiantes)
+    # print(nombre)
     return f"El estudiante a registrar es: {nombre} y su correo es: {correo}"
+
+# Parametros en la ruta
+@app.route("/estudiantes/<string:area>/<int:grupo>")
+def mostrar_estudiantes(area, grupo):
+    return f"El programa de formación consultado es {area} y el grupo consultado es la variable {grupo}"
+
+# Solicitud tipo 4
+@app.route('/ver-headers')
+def ver_headers():
+    agente_usuario = request.headers.get('User-Agent')
+    return f"Tu navegador es: {agente_usuario}"
+
+# Gestion de cookies
+@app.route('/crear-cookie')
+def crear_cookie():
+    respuesta = make_response("Cookie creada!") #Es un mensaje de respuesta
+    respuesta.set_cookie('usuario_logueado', 'true')
+    return respuesta
+
+@app.route('/leer-cookie')
+
+def leer_cookie():
+    valor = request.cookies.get('usuario_logueado')
+    return f"Valor de la cookie: {valor}"
 
 
 
